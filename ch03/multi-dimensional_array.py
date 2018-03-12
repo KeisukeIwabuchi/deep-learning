b72a32d6847ff3ac3da9bdcacd8a6c52fc8433a8
Keisuke Iwabuchi <cola.ice.puyo@gmail.com>
2018-03-12 13:52:32 +0900

    add file
    numpyを使った行列の計算

---
+++ ch03/multi-dimensional_array.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+A = np.array([1, 2, 3, 4])
+
+# print(A)
+# print(np.ndim(A))
+# print(A.shape)
+# print(A.shape[0])
+
+
+B = np.array([[1, 2], [3, 4], [5, 6]])
+
+# print(B)
+# print(np.ndim(B))
+# print(B.shape)
+
+
+# 行列の積
+A = np.array([[1, 2], [3, 4]])
+print(A.shape)
+
+B = np.array([[5, 6], [7, 8]])
+print(B.shape)
+
+print(np.dot(A, B))
