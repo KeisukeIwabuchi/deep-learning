66a1593fc31be7504450ee66a336b5c253fa8ae5
Keisuke Iwabuchi <cola.ice.puyo@gmail.com>
2018-03-16 14:04:06 +0900

    add gradient descent method

--- ch04/numerical_differentiation.py
+++ ch04/numerical_differentiation.py
@@ -1,20 +1,40 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-def numerical_deff(f, x):
+def numerical_gradient(f, x):
   h = 1e-4 # 0.0001
-  return (f(x+h) - f(x-h)) / (2 * h)
+  grad = np.zeros_like(x)
+
+  for idx in range(x.size):
+    tmp_val = x[idx]
+    x[idx] = tmp_val + h
+    fxh1 = f(x)
+
+    x[idx] = tmp_val - h
+    fxh2 = f(x)
+
+    grad[idx] = (fxh1 - fxh2) / (2 * h)
+    x[idx] = tmp_val
+
+  return grad
 
 def function_1(x):
   return 0.01 * (x**2) + 0.1 * x
 
-x = np.arange(0.0, 20.0, 0.1)
-y = function_1(x)
-# plt.xlabel("x")
-# plt.ylabel("f(x)")
-# plt.plot(x, y)
-# plt.show()
+def function_2(x):
+  return x[0]**2 + x[1]**2
 
-print(numerical_deff(function_1, 5))
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+  x = init_x
 
-print(numerical_deff(function_1, 10))
+  for i in range(step_num):
+    grad = numerical_gradient(f, x)
+    x -= lr * grad
+  
+  return x
+
+
+
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
+
